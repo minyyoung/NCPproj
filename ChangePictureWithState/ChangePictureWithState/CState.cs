81825243ab39c1e3216129a99a8b54a8c19c0024
Hajin <gjgkwls@naver.com>
2016-05-23 21:00:01 +0900

    .

---
+++ ChangePictureWithState/ChangePictureWithState/CState.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows.Forms;
+
+namespace ChangePictureWithState
+{
+    class CState
+    {
+        private PictureBox img;
+        private int state;
+
+        public CState(ref PictureBox image)
+        {
+            img = image;
+        }
+        public int State
+        {
+            get
+            {
+                return state;
+            }
+            set
+            {
+                // if state is change!?
+                // change image.
+                state = value;
+                img.Load("C:\\Users\\Student\\Desktop\\b.jpg");
+            }
+        }
+    }
+}
---
+++ ChangePictureWithState/ChangePictureWithState/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChangePictureWithState
+{
+    public partial class Form1 : Form
+    {
+        CState cs;
+        public Form1()
+        {
+            InitializeComponent();
+
+            pictureBox.Load("C:\\Users\\Student\\Desktop\\a.jpg");
+            pictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            cs = new CState(ref pictureBox);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            cs.State = cs.State + 10;
+            labelState.Text = cs.State.ToString();
+        }
+    }
+}
