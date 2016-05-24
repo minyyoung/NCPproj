a2ec13a680d662a738c1b3ba0787450b046ab5e7
Hajin Heo <gjgkwls@naver.com>
2016-05-24 14:27:10 +0900

    teacher project

---
+++ teacher/weatherAPIExam/weatherAPIExam/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace weatherAPIExam
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose( bool disposing )
+        {
+            if( disposing && (components != null) )
+            {
+                components.Dispose();
+            }
+            base.Dispose( disposing );
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbPage = new System.Windows.Forms.TextBox();
+            this.tbResult = new System.Windows.Forms.TextBox();
+            this.cbCode = new System.Windows.Forms.ComboBox();
+            this.cbKind1 = new System.Windows.Forms.ComboBox();
+            this.cbKind2 = new System.Windows.Forms.ComboBox();
+            this.cbKind3 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.areaNumber = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(503, 379);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(72, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // tbPage
+            // 
+            this.tbPage.Location = new System.Drawing.Point(397, 381);
+            this.tbPage.Name = "tbPage";
+            this.tbPage.Size = new System.Drawing.Size(100, 21);
+            this.tbPage.TabIndex = 2;
+            // 
+            // tbResult
+            // 
+            this.tbResult.Location = new System.Drawing.Point(54, 133);
+            this.tbResult.Multiline = true;
+            this.tbResult.Name = "tbResult";
+            this.tbResult.ReadOnly = true;
+            this.tbResult.Size = new System.Drawing.Size(327, 139);
+            this.tbResult.TabIndex = 3;
+            // 
+            // cbCode
+            // 
+            this.cbCode.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbCode.FormattingEnabled = true;
+            this.cbCode.Location = new System.Drawing.Point(245, 381);
+            this.cbCode.Name = "cbCode";
+            this.cbCode.Size = new System.Drawing.Size(121, 20);
+            this.cbCode.TabIndex = 4;
+            this.cbCode.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // cbKind1
+            // 
+            this.cbKind1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbKind1.FormattingEnabled = true;
+            this.cbKind1.Location = new System.Drawing.Point(54, 303);
+            this.cbKind1.Name = "cbKind1";
+            this.cbKind1.Size = new System.Drawing.Size(121, 20);
+            this.cbKind1.TabIndex = 5;
+            this.cbKind1.SelectedIndexChanged += new System.EventHandler(this.cbKind1_SelectedIndexChanged);
+            // 
+            // cbKind2
+            // 
+            this.cbKind2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbKind2.FormattingEnabled = true;
+            this.cbKind2.Location = new System.Drawing.Point(181, 303);
+            this.cbKind2.Name = "cbKind2";
+            this.cbKind2.Size = new System.Drawing.Size(121, 20);
+            this.cbKind2.TabIndex = 6;
+            this.cbKind2.SelectedIndexChanged += new System.EventHandler(this.cbKind2_SelectedIndexChanged);
+            // 
+            // cbKind3
+            // 
+            this.cbKind3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbKind3.FormattingEnabled = true;
+            this.cbKind3.Location = new System.Drawing.Point(308, 303);
+            this.cbKind3.Name = "cbKind3";
+            this.cbKind3.Size = new System.Drawing.Size(121, 20);
+            this.cbKind3.TabIndex = 7;
+            this.cbKind3.SelectedIndexChanged += new System.EventHandler(this.cbKind3_SelectedIndexChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(590, 83);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Load";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // areaNumber
+            // 
+            this.areaNumber.Location = new System.Drawing.Point(445, 302);
+            this.areaNumber.Name = "areaNumber";
+            this.areaNumber.Size = new System.Drawing.Size(100, 21);
+            this.areaNumber.TabIndex = 9;
+            this.areaNumber.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(739, 500);
+            this.Controls.Add(this.areaNumber);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.cbKind3);
+            this.Controls.Add(this.cbKind2);
+            this.Controls.Add(this.cbKind1);
+            this.Controls.Add(this.cbCode);
+            this.Controls.Add(this.tbResult);
+            this.Controls.Add(this.tbPage);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbPage;
+        private System.Windows.Forms.TextBox tbResult;
+        private System.Windows.Forms.ComboBox cbCode;
+        private System.Windows.Forms.ComboBox cbKind1;
+        private System.Windows.Forms.ComboBox cbKind2;
+        private System.Windows.Forms.ComboBox cbKind3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox areaNumber;
+    }
+}
+
---
+++ teacher/weatherAPIExam/weatherAPIExam/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Web;
+using System.Net;
+
+namespace weatherAPIExam
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            /* to be continue...
+            theCode = new SortedList<string, string>();
+            theCode.Add( "청운효자동", "1111051500" );
+            theCode.Add( "사직동", "1111053000" );
+
+            foreach( KeyValuePair<string, string> iter in theCode )
+            {
+                cbCode.Items.Add( iter.Key );
+            }
+
+//            cbCode.Items.Add( "1111000000" );
+//            cbCode.Items.Add( "1111051500" );*/
+
+            theAM = new CAreaManager();
+/*            theAM.AddArea("서울", "종로구", "청운효자동", "1111051500");
+            theAM.AddArea("서울", "종로구", "혜화동", "1111065000");
+            theAM.AddArea("서울", "종로구", "-", "1111000000");
+            theAM.AddArea("서울", "도봉구", "창제1동", "1132051100");
+            theAM.AddArea("서울", "관악구", "대학동", "1162073500");
+            theAM.AddArea("서울", "관악구", "신림동", "1162069500");
+            theAM.AddArea("서울", "관악구", "보라매동", "1162052500");
+            theAM.AddArea("서울", "동작구", "신대방제2동", "1159068000");
+            theAM.AddArea("전라북도", "군산시", "소룡동", "4513071000");
+            theAM.AddArea("전라북도", "군산시", "월명동", "4513053000");
+            theAM.AddArea("광주광역시", "동구", "충장동", "2911052500");
+            theAM.AddArea("전라북도", "전주시덕진구", "진북동", "4511352500");
+            theAM.AddArea("부산광역시", "영도구", "청학제1동", "2620063000");
+
+            cbKind1.Items.Clear();
+            foreach (KeyValuePair<string, SortedList<string, SortedList<string, string>> > iter in theAM.theAreas)
+            {
+                cbKind1.Items.Add(iter.Key);
+            }
+            cbKind1.SelectedIndex = 0;
+*/
+            /*            cbCode.Items.Add( "1111053000" );
+                      cbCode.Items.Add( "1111054000" );
+                      cbCode.Items.Add( "1111055000" );
+                      cbCode.Items.Add( "1111056000" );
+                      cbCode.Items.Add( "1111057000" );
+                      cbCode.Items.Add( "1111058000" );
+                     cbCode.SelectedIndex = 0;
+          */        }
+
+        string selectID2;
+        string selectID3;
+
+        private void cbKind1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            selectID2 = cbKind1.Items[cbKind1.SelectedIndex].ToString();
+            SortedList<string, SortedList<string, string>> tmpArea2 = theAM.theAreas[selectID2];
+
+            cbKind2.Items.Clear();
+            foreach (KeyValuePair<string, SortedList<string, string>> iter in tmpArea2)
+            {
+                cbKind2.Items.Add(iter.Key);
+            }
+            cbKind2.SelectedIndex = 0;
+        }
+
+        private void cbKind2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            selectID3 = cbKind2.Items[cbKind2.SelectedIndex].ToString();
+            SortedList<string, string> tmpArea3 = theAM.theAreas[selectID2][selectID3];
+
+            cbKind3.Items.Clear();
+            foreach (KeyValuePair<string, string> iter in tmpArea3)
+            {
+                cbKind3.Items.Add(iter.Key);
+            }
+            cbKind3.SelectedIndex = 0;
+        }
+
+        public SortedList<string, string> theCode;
+
+        private void button1_Click( object sender, EventArgs e )
+        {
+//            HttpWebRequest http = (HttpWebRequest) WebRequest.Create(
+//                "http://203.247.66.146/iros/RetrieveWhoIndexService/getAsthmaWhoList?ServiceKey=6SWK3Ll%2BXniV1bEC%2F06SPf8PiL4vbC8sCX%2BcmuMr1VDOCVaiD%2F%2FWpqOpM0KhfIg4%2Fi6KUeRyJVcDktuYuOKOsg%3D%3D&AreaNo=1100000000&numOfRows=999&pageSize=999&pageNo=1&startPage=1" );
+            HttpWebRequest http = (HttpWebRequest) WebRequest.Create(
+                "http://203.247.66.146/iros/RetrieveWhoIndexService/getAsthmaWhoList?ServiceKey="
+                + "6SWK3Ll%2BXniV1bEC%2F06SPf8PiL4vbC8sCX%2BcmuMr1VDOCVaiD%2F%2FWpqOpM0KhfIg4%2Fi6KUeRyJVcDktuYuOKOsg%3D%3D&"
+                + "AreaNo=" + areaNumber.Text + "&numOfRows=999&pageSize=999&pageNo="+ tbPage.Text + "&startPage=1" );
+
+//           string tmpCode = theCode[ cbCode.SelectedItem.ToString() ];
+
+            WebResponse response = http.GetResponse();
+            StreamReader sr = new StreamReader( response.GetResponseStream() );
+
+//            MemoryStream stream = response.GetResponseStream();
+//            StreamReader sr = new StreamReader( stream );
+            string content = sr.ReadToEnd();
+            tbResult.Text = content;
+        }
+
+        private void comboBox1_SelectedIndexChanged( object sender, EventArgs e )
+        {
+
+        }
+
+        CAreaManager theAM;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click ( object sender, EventArgs e )
+        {
+            theAM.theAreas.Clear();
+
+            StreamReader tmpSR = new StreamReader(new FileStream("address.csv", FileMode.Open));
+            while(tmpSR.EndOfStream == false)
+            {
+                String tmpStr = tmpSR.ReadLine();
+                String[] tmpClass = tmpStr.Split( ',' );
+
+
+                if( tmpClass[2] == "")
+                {
+                    theAM.AddArea(tmpClass[1], "-", "-", tmpClass[0]);
+                }
+                else if( tmpClass[3] == "" )
+                {
+                    theAM.AddArea(tmpClass[1], tmpClass[2], "-", tmpClass[0]);
+                }
+                else
+                {
+                    theAM.AddArea(tmpClass[1], tmpClass[2], tmpClass[3], tmpClass[0]);
+                }
+
+
+            }
+            cbKind1.Items.Clear();
+            foreach(KeyValuePair<string, SortedList<string, SortedList<string, string>>> iter in theAM.theAreas)
+            {
+                cbKind1.Items.Add(iter.Key);
+            }
+            cbKind1.SelectedIndex = 0;
+            tmpSR.Close();
+        }
+
+        private void textBox1_TextChanged ( object sender, EventArgs e )
+        {
+
+        }
+
+        private void cbKind3_SelectedIndexChanged ( object sender, EventArgs e )
+        {
+            string tmpselectID2 = cbKind1.Items[cbKind1.SelectedIndex].ToString();
+            string tmpselectID3 = cbKind2.Items[cbKind2.SelectedIndex].ToString();
+            string tmpselectID4 = cbKind3.Items[cbKind3.SelectedIndex].ToString();
+
+            areaNumber.Text = theAM.theAreas[tmpselectID2][tmpselectID3][tmpselectID4];
+        }
+
+
+
+
+    }
+}
