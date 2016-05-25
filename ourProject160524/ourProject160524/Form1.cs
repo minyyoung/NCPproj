d8d694b454d305ea869e8e5e87b640d05fa950db
Hajin <gjgkwls@naver.com>
2016-05-25 16:53:50 +0900

    d

--- ourProject160524/ourProject160524/Form1.cs
+++ ourProject160524/ourProject160524/Form1.cs
@@ -10,6 +10,7 @@ using System.Windows.Forms;
 using System.IO;
 using System.Web;
 using System.Net;
+using System.Xml;
 
 namespace ourProject160524
 {
@@ -95,9 +96,7 @@ namespace ourProject160524
         private void button1_Click( object sender, EventArgs e )
         {
             HttpWebRequest http = (HttpWebRequest) WebRequest.Create(
-                "http://203.247.66.146/iros/RetrieveWhoIndexService/getAsthmaWhoList?ServiceKey="
-                + "6SWK3Ll%2BXniV1bEC%2F06SPf8PiL4vbC8sCX%2BcmuMr1VDOCVaiD%2F%2FWpqOpM0KhfIg4%2Fi6KUeRyJVcDktuYuOKOsg%3D%3D&"
-                + "AreaNo=" + areaNumber.Text + "&numOfRows=999&pageSize=999&pageNo=10&startPage=1" );
+                "http://203.247.66.146/iros/RetrieveLifeIndexService/getUltrvLifeList?ServiceKey=6SWK3Ll%2BXniV1bEC%2F06SPf8PiL4vbC8sCX%2BcmuMr1VDOCVaiD%2F%2FWpqOpM0KhfIg4%2Fi6KUeRyJVcDktuYuOKOsg%3D%3D&AreaNo=1100000000&numOfRows=999&pageNo=1" );
 
 
             WebResponse response = http.GetResponse();
@@ -105,6 +104,27 @@ namespace ourProject160524
 
             string content = sr.ReadToEnd();
             tbResult.Text = content;
+
+            parsingXML(content);
+        }
+
+        private void parsingXML(string strXml)
+        {
+            XmlDocument xml = new XmlDocument(); // XmlDocument 생성
+            xml.LoadXml(strXml);
+            XmlNode code = xml.GetElementsByTagName("code")[0];
+            XmlNode areaNo = xml.GetElementsByTagName("areaNo")[0];
+            XmlNode date = xml.GetElementsByTagName("date")[0];
+            XmlNode today = xml.GetElementsByTagName("today")[0];
+            XmlNode tomorrow = xml.GetElementsByTagName("tomorrow")[0];
+            XmlNode afterTo = xml.GetElementsByTagName("theDayAfterTomorrow")[0];
+
+            parsingText.Text = "code : " + code.InnerText + "\n";
+            parsingText.AppendText("areaNo : " + areaNo.InnerText + "\n");
+            parsingText.AppendText("date : " + date.InnerText + "\n");
+            parsingText.AppendText("today : " + today.InnerText + "\n");
+            parsingText.AppendText("tomorrow : " + tomorrow.InnerText + "\n");
+            parsingText.AppendText("afterTo : " + afterTo.InnerText + "\n");
         }
 
         private void cbKind3_SelectedIndexChanged( object sender, EventArgs e )
